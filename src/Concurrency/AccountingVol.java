f7e44a385b8521282b62d357b4449b493c89ae81
science09 <luozhaofeng86@163.com>
2017-02-23 23:03:18 +0800

    dd

--- src/Concurrency/AccountingVol.java
+++ src/Concurrency/AccountingVol.java
@@ -1,5 +1,7 @@
 package Concurrency;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by hadoop on 17-2-21.
  * 多线程下对一个变量进行累加
@@ -7,11 +9,20 @@ package Concurrency;
 
 public class AccountingVol implements Runnable {
 
+    private static ReentrantLock lock = new ReentrantLock();
     private static AccountingVol instance = new AccountingVol();
     private static volatile int count = 0;
 
-    private static synchronized void increase() {
-        count++;
+    //synchronized
+    private static void increase() {
+
+//        lock.lock();
+//        count++;
+//        lock.unlock();
+
+        synchronized (AccountingVol.class) {
+            count++;
+        }
     }
 
     @Override
@@ -24,12 +35,15 @@ public class AccountingVol implements Runnable {
     public static void main(String[] args) {
 
         try {
+            Long startTime = System.currentTimeMillis();
             Thread t1 = new Thread(instance);
             Thread t2 = new Thread(instance);
             t1.start();
             t2.start();
             t1.join();
             t2.join();
+            Long endTime = System.currentTimeMillis();
+            System.out.println("ElapseTime: " + (endTime - startTime) + " ms");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
